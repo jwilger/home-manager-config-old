8afbddd1c05152fbbcc95a45e9bc7544f854713f
John Wilger <john@johnwilger.com>
2024-06-12 13:49:01 -0700

    Add basic git config

--- home.nix
+++ home.nix
@@ -9,4 +9,12 @@
         homeDirectory = if pkgs.stdenv.isLinux then "/home/jwilger" else "/Users/jwilger";
         stateVersion = "23.11";
     };
+
+    programs = {
+        git = {
+            enable = true;
+            userName = "John Wilger";
+            userEmail = "john@johnwilger.com";
+        };
+    };
 }
