49d261e110c10f739208e3b1fbe4df444a93d342
John Wilger <john@johnwilger.com>
2024-06-13 07:50:54 -0700

    Fix formatting of common.nix

--- home/common.nix
+++ home/common.nix
@@ -9,57 +9,67 @@
         stateVersion = "23.11";
     };
 
+    neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
+        withNodeJs = true;
+        withPython3 = true;
+    };
+
     programs = {
         git = {
             enable = true;
             userName = "John Wilger";
             userEmail = "john@johnwilger.com";
         };
-    starship = {
-        enable = true;
-        enableZshIntegration = true;
-    };
-    zsh = {
-        enable = true;
-        defaultKeymap = "vicmd";
-        enableCompletion = true;
-	history = {
-            ignoreDups = true;
-            share = true;
-	};
-        oh-my-zsh = {
+        starship = {
             enable = true;
-        plugins = [ "git" "sudo" "keychain" "direnv" "mix" "pyenv" ];
+            enableZshIntegration = true;
         };
+        zsh = {
+            enable = true;
+            defaultKeymap = "vicmd";
+            enableCompletion = true;
+            history = {
+                ignoreDups = true;
+                share = true;
+            };
+            oh-my-zsh = {
+                enable = true;
+            plugins = [ "git" "sudo" "keychain" "direnv" "mix" "pyenv" ];
+            };
             shellAliases = {
-            # Git
-            gst="git status";
-            gci="git commit";
-            gap="git add --patch";
+                # Git
+                gst="git status";
+                gci="git commit";
+                gap="git add --patch";
 
-            # OS
-            ls="ls -lGh";
-            envs="env | sort";
-            envg="env | grep -i";
+                # OS
+                ls="ls -lGh";
+                envs="env | sort";
+                envg="env | grep -i";
 
-            vim="nvim";
-            vi="nvim";
+                vim="nvim";
+                vi="nvim";
 
-            # Random
-            guid="uuidgen | tr \"[:upper:]\" \"[:lower:]\"";
-            publicip="dig +short myip.opendns.com @resolver1.opendns.com";
-            localip="ifconfig | grep \"inet \" | grep -v 127.0.0.1 | cut -d\\  -f2";
+                # Random
+                guid="uuidgen | tr \"[:upper:]\" \"[:lower:]\"";
+                publicip="dig +short myip.opendns.com @resolver1.opendns.com";
+                localip="ifconfig | grep \"inet \" | grep -v 127.0.0.1 | cut -d\\  -f2";
 
-            # tmuxinator
-            tma="tmux attach";
-            tm="tmuxinator start";
+                # tmuxinator
+                tma="tmux attach";
+                tm="tmuxinator start";
 
-            # GitHub CLI
-            ghr="gh run watch";
-        };
-        syntaxHighlighting = {
-            enable = true;
-        };
+                # GitHub CLI
+                ghr="gh run watch";
+            };
+            syntaxHighlighting = {
+                enable = true;
+            };
         };
     };
 }
