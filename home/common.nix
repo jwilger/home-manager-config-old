200734fd205eaf8a856867defb5ac8370598213d
John Wilger <john@johnwilger.com>
2024-06-15 16:22:38 -0700

    Fix gpg-agent service to run only on linux

--- home/common.nix
+++ home/common.nix
@@ -20,20 +20,6 @@
         };
     };
 
-    services = {
-        gpg-agent = {
-            enable = true;
-            defaultCacheTtl = 3600;
-            maxCacheTtl = 36000;
-            defaultCacheTtlSsh = 3600;
-            maxCacheTtlSsh = 36000;
-            enableSshSupport = true;
-            extraConfig = ''
-            allow-preset-passphrase
-            '';
-        };
-    };
-
     programs = {
         git = {
             enable = true;
--- home/linux.nix
+++ home/linux.nix
@@ -4,4 +4,18 @@
         homeDirectory = "/home/jwilger";
         stateVersion = "23.11";
     };
+
+    services = {
+        gpg-agent = {
+            enable = true;
+            defaultCacheTtl = 3600;
+            maxCacheTtl = 36000;
+            defaultCacheTtlSsh = 3600;
+            maxCacheTtlSsh = 36000;
+            enableSshSupport = true;
+            extraConfig = ''
+            allow-preset-passphrase
+            '';
+        };
+    };
 }
