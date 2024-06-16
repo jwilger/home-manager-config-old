f6dea80bcd7eca7db30e626f4f9a799112c0972f
John Wilger <john@johnwilger.com>
2024-06-15 18:53:33 -0700

    Use 1password as SSH agent on macos

--- home/darwin.nix
+++ home/darwin.nix
@@ -4,4 +4,13 @@
         homeDirectory = "/Users/jwilger";
         stateVersion = "23.11";
     };
+
+    programs = {
+        ssh = {
+            extraConfig = ''
+            Host *
+                IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+            '';
+        };
+    };
 }
