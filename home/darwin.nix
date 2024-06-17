0d8a7a3efb8626e9db4be1defe9a578bdc4cdc54
John Wilger <john@johnwilger.com>
2024-06-17 08:43:56 -0700

    Add vscode on macos systems

--- home/darwin.nix
+++ home/darwin.nix
@@ -20,5 +20,9 @@
                 IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
             '';
         };
+
+        vscode = {
+            enable = true;
+        };
     };
 }
