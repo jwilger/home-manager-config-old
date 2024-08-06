88127715d841cd1c441e05e646332ba0b8f075d9
John Wilger <john@johnwilger.com>
2024-08-05 21:56:58 -0700

    Numerous changes as I was getting a new Ubuntu system set up

--- home/linux.nix
+++ home/linux.nix
@@ -1,18 +1,20 @@
 { lib, pkgs, ... }:
 {
-    home = {
-        homeDirectory = "/home/jwilger";
-        stateVersion = "23.11";
-    };
+  home = {
+    homeDirectory = "/home/jwilger";
+    stateVersion = "23.11";
+  };
 
-    services = {
-        gpg-agent = {
-            enable = true;
-            defaultCacheTtl = 3600;
-            maxCacheTtl = 36000;
-            extraConfig = ''
-            allow-preset-passphrase
-            '';
-        };
+  services = {
+    gpg-agent = {
+      enable = true;
+      defaultCacheTtl = 3600;
+      maxCacheTtl = 36000;
+      extraConfig = ''
+        allow-preset-passphrase
+      '';
     };
+  };
+
+  xdg.configFile."regolith3/Xresources".source = ../Xresources;
 }
